2470e8bbbdbda966da5d2bc61e90331b67f8051e
Sheerland <sherlanelan@gmail.com>
2018-07-17 23:35:39 +0800

    updates 80%

--- Portfolio/assets/javascript/script.js
+++ Portfolio/assets/javascript/script.js
@@ -21,6 +21,7 @@ $(document).ready(function(){
 	    .addClass('active')
 	    .siblings();
 	}
+  
 	$('.nav-bar').find('.nav-bar-content h2').click(function(){
 		$('.about-me').animate({
 			top: 0
@@ -29,6 +30,37 @@ $(document).ready(function(){
 	$('.footer h2').click(function(){
 		$('.my-strength').animate({
 			bottom: 0
+		});
+	});
+	$('.left-content-text h2').click(function(){
+		$('.portfolio').animate({
+			left:0
+		});
+	});
+	$('.right-content-text h2').click(function(){
+		$('.contact').animate({
+			right:0
+		});
+	});
+	$('div#close-about-me').click(function(){
+		$('.about-me').animate({
+			top: "-100%"
+		});
+	});
+	$('div#close-strength').click(function(){
+		$('.my-strength').animate({
+			bottom: "-100%"
+		});
+	});
+	$('div#close-portfolio').click(function(){
+		$('.portfolio').animate({
+			left: "-100%"
+		});
+	});
+
+	$('div#close-contact').click(function(){
+		$('.contact').animate({
+			right: "-100%"
 		});
 	});
 });
